aacbea7faa55a1fcc2363ecc4edf3b4d647ca4d6
juliasat <58359447+juliasatt@users.noreply.github.com>
2024-11-08 11:03:28 -0300

    more stuff

--- plugins/iq_manipulation_support/iq/module_iq_re.cpp
+++ plugins/iq_manipulation_support/iq/module_iq_re.cpp
@@ -1,9 +1,12 @@
 #include "module_iq_re.h"
 #include "common/dsp/buffer.h"
 #include "common/dsp/io/file_sink.h"
+#include "common/dsp/io/wav_writer.h"
 #include "common/dsp/path/splitter.h"
 #include "common/dsp/resamp/rational_resampler.h"
 #include "common/utils.h"
+#include "common/ziq.h"
+#include "common/ziq2.h"
 #include "core/module.h"
 #include <cstdint>
 #include <memory>
@@ -62,11 +65,39 @@ namespace iq_re
         //if (d_parameters["output_format"].get<std::string>() == "ziq2")
         //    output_format = "ziq2";
 
-        //if (output_data_type == DATA_FILE)
-        //{
-        //    data_out = std::ofstream(d_output_file_hint + "." + output_format, std::ios::binary);
-        //    d_output_files.push_back(d_output_file_hint + "." + output_format);
-        //}
+        if (output_data_type == DATA_FILE)
+        {
+            data_out = std::ofstream(d_output_file_hint + "." + output_format, std::ios::binary);
+            d_output_files.push_back(d_output_file_hint + "." + output_format);
+        }
+
+        if (output_format == "wav")
+        {
+            dsp::WavWriter wave_writer(data_out);
+            wave_writer.write_header(res_samplerate, 2);
+        }
+
+//#ifdef BUILD_ZIQ
+//        if (output_format == "ziq")
+//        {
+//            ziqcfg.is_compressed = true;
+//            ziqcfg.bits_per_sample = output_format.ziq_depth;
+//            ziqcfg.samplerate = res_samplerate;
+//            ziqcfg.annotation = "";
+//
+//            ziqWriter = std::make_shared<ziq::ziq_writer>(ziqcfg, data_out);
+//        }
+//#endif
+//#ifdef BUILD_ZIQ2
+//        if (output_format == "ziq2")
+//        {
+//            int sz = ziq2::ziq2_write_file_hdr((uint8_t *)buffer_s8, samplerate);
+//            data_out.write((char *)buffer_s8, sz);
+//
+//            if (mag_buffer == nullptr)
+//                mag_buffer == dsp::create_volk_buffer<float>(d_buffer_size);
+//        }
+//#endif
 
         logger->info("Using input baseband " + d_input_file);
         logger->info("Converting to " + d_output_file_hint + output_format);
@@ -75,6 +106,7 @@ namespace iq_re
         time_t lastTime = 0;
 
         uint64_t final_data_size = 0;
+        uint64_t final_data_size_raw = 0;
         complex_t last_samp;
 
         // Start
@@ -90,8 +122,6 @@ namespace iq_re
 
         int8_t * buffer_s8 = dsp::create_volk_buffer<int8_t>(d_buffer_size * 2);
         int16_t * buffer_s16 = dsp::create_volk_buffer<int16_t>(d_buffer_size * 2);
-
-
 
         int dat_size = 0;
         while (demod_should_run())
@@ -153,13 +183,21 @@ namespace iq_re
                     final_data_size += dat_size * sizeof(int8_t) * 2;
                 }
 //#ifdef BUILD_ZIQ
-                //else if (output_format == "ziq")
-                //{
-                //    final_data_size += 
-                //}
+//                else if (output_format == "ziq")
+//                {
+//                    final_data_size += ziqWriter->write(work_buffer_complex, dat_size);
+//                    final_data_size_raw += (ziqcfg.bits_per_sample / 4) * dat_size;
+//                }
+//#endif
+//#ifdef BUILD_ZIQ2
+//                else if (output_format == "ziq2")
+//                {
+//                    int sz = ziq2::ziq2_write_iq_pkt((uint8_t *)buffer_s8, work_buffer_complex, mag_buffer, dat_size, output_format.ziq_depth);
+//                    data_out.write((char *)buffer_s8, sz);
+//                    final_data_size += sz;
+//                }
+//#endif
             }
-
-
 
             //if (output_data_type == DATA_FILE)
             //{
--- plugins/iq_manipulation_support/iq/module_iq_re.h
+++ plugins/iq_manipulation_support/iq/module_iq_re.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "common/dsp/resamp/rational_resampler.h"
+#include "common/ziq.h"
 #include "modules/demod/module_demod_base.h"
 #include <cstdint>
 #include <memory>
@@ -21,6 +22,15 @@ namespace iq_re
         uint64_t frequency_hz = 0;
 
         std::mutex out_mtx;
+
+#ifdef BUILD_ZIQ
+        ziq::ziq_cfg ziqcfg;
+        std::shared_ptr<ziq::ziq_writer> ziqWriter;
+#endif
+
+        float *mag_buffer = nullptr;
+
+        std::unique_ptr<dsp::WavWriter> wav_writer;
 
     public:
         IQREModule(std::string input_file, std::string output_file_hint, nlohmann::json parameters);
